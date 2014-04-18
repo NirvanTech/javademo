f1ad8d56b0644de0ffc53af6eaee1e45133f5046
vparupati <vparupati@gmail.com>
2014-04-18 12:41:35 +0100

    osgi first

---
+++ com.demo.osgi/src/main/java/com/demo/osgi/Activator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.demo.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author sagarreddy
+ *
+ */
+public class Activator implements BundleActivator{
+
+	public void start(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
